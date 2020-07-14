2b563463e0e03543a5744ca2d8d15b24de98b36e
Johnny Rebollar <johnnyrebollar@lewisu.edu>
2020-07-14 16:23:01 -0500

    Create GradeAvg.java

---
+++ GradeAvg.java
@@ -0,0 +1,98 @@
+
+public class GradeAvg {
+
+	public static void main(String[] args) {
+		int n=2,result=0;
+		
+		int a[]=new int[5];
+		
+		a[0]=85;
+		a[1]=94;
+		
+		
+		
+		for(int i=0;i<n;i++)
+			result=result+a[i];
+		
+		System.out.println("average of ("+a[0]+","+a[1]+") is ="+result/n);
+		
+		int n1=2,result1=0; 
+		int a1[]=new int[5];
+		
+		a1[0]=60;
+		a1[1]=80;
+		
+		
+		
+		for(int i=0;i<n1;i++)
+			result1=result1+a1[i];
+		
+		System.out.println("average of ("+a1[0]+","+a1[1]+") is ="+result1/n1);
+		
+		int n11=2,result11=0; 
+		int a11[]=new int[5];
+		
+		a11[0]=89;
+		a11[1]=89;
+		
+		
+		
+		for(int i=0;i<n11;i++)
+			result11=result11+a11[i];
+		
+		System.out.println("average of ("+a11[0]+","+a11[1]+") is ="+result11/n11);
+		
+		int n111=2,result111=0; 
+		int a111[]=new int[5];
+		
+		a111[0]=52;
+		a111[1]=93;
+		
+		
+		
+		for(int i=0;i<n111;i++)
+			result111=result111+a111[i];
+		
+		System.out.println("average of ("+a111[0]+","+a111[1]+") is ="+result111/n111);
+		
+		int n1111=2,result1111=0; 
+		int a1111[]=new int[5];
+		
+		a1111[0]=40;
+		a1111[1]=90;
+		
+		
+		
+		for(int i=0;i<n1111;i++)
+			result1111=result1111+a1111[i];
+		
+		System.out.println("average of ("+a1111[0]+","+a1111[1]+") is ="+result1111/n1111);
+		
+		int n11111=2,result11111=0; 
+		int a11111[]=new int[5];
+		
+		a11111[0]=82;
+		a11111[1]=80;
+		
+		
+		
+		for(int i=0;i<n11111;i++)
+			result11111=result11111+a11111[i];
+		
+		System.out.println("average of ("+a11111[0]+","+a11111[1]+") is ="+result11111/n11111);
+		
+		int n111111=2,result111111=0; 
+		int a111111[]=new int[5];
+		
+		a111111[0]=88;
+		a111111[1]=81;
+		
+		
+		
+		for(int i=0;i<n111111;i++)
+			result111111=result111111+a111111[i];
+		
+		System.out.println("average of ("+a111111[0]+","+a111111[1]+") is ="+result111111/n111111);
+	}
+
+}
